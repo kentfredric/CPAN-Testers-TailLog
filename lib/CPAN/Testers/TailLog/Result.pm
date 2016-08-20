9ca7f7163e3ee6aa3bd0403906afc282acb071c8
Kent Fredric <kentfredric@gmail.com>
2016-08-20 22:58:01 +1200

    Fill in Result Accessors

--- lib/CPAN/Testers/TailLog/Result.pm
+++ lib/CPAN/Testers/TailLog/Result.pm
@@ -23,34 +23,77 @@ sub uuid        { $_[0]->{uuid} }
 
 1;
 
+=pod
+
+=encoding UTF-8
+
 =head1 NAME
 
 CPAN::Testers::TailLog::Result - A single log entry from
 metabase.cpantesters.org
 
-=head1 SYNOPSIS
-
 =head1 DESCRIPTION
+
+All propteries in this object are verbatim strings from upstream, with unicode
+text (not bytes) where relevant.
 
 =head1 METHODS
 
-=head2 new
-
 =head2 accepted
+
+The time the report was accepted to C<Metabase>, verbatim.
+
+  # {YYYY}-{MM}-{DD}T{HH}:{MM}:{SS}Z
+  my $time = $result->accepted
 
 =head2 filename
 
+The CPAN relative path to the filename the test report is for
+
+  # {CPANAUTHOR}/{PATH}
+  my $text = $result->filename
+
 =head2 grade
+
+The status of the test report
+
+  # pass|fail|na|unknown
+  my $grade = $result->grade
 
 =head2 perlversion
 
+The version of Perl the test ran on
+
+  # perl-vX.YY.Z
+  my $pv = $result->perlversion
+
 =head2 platform
+
+The OS/Architecture the test ran on
+
+  # eg: x86_64-gnukfreebsd
+  my $pf = $result->platform
 
 =head2 reporter
 
+The person who submitted the report
+
+  # "AuthörName"
+  my $name = $result->reporter;
+
 =head2 submitted
 
+The submission time of the report
+
+  # {YYYY}-{MM}-{DD}T{HH}:{MM}:{SS}Z
+  my $time = $result->submitted
+
 =head2 uuid
+
+The unique identifier of the report
+
+  # {HEX(8)}-{HEX(4)}-{HEX(4)}-{HEX(4)}-{HEX(12)}
+  my $id = $result->uuid
 
 =head1 AUTHOR
 
